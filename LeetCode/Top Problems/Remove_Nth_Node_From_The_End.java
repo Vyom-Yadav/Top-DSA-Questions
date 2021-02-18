5b57912af1b948848a727945c8f7d1d5d059fe36
Vyom Yadav <73882557+Vyom-Yadav@users.noreply.github.com>
2021-02-18 23:52:59 +0530

    new

---
+++ LeetCode/Top Problems/Remove_Nth_Node_From_The_End.java
@@ -0,0 +1,73 @@
+/*
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Follow up: Could you do this in one pass?
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+ */
+// Before watching solution try on https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+// Solution
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Remove_Nth_Node_From_The_End {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null || head.next == null){
+            return null;
+        }
+        ListNode newHead = reverseList(head);
+        ListNode temp = newHead;
+        int i = 1;
+        if(n == 1){
+            newHead = newHead.next;
+            return reverseList(newHead);
+        }
+        while(i < n-1){
+            temp = temp.next;
+            i++;
+        }
+        temp.next = temp.next.next;
+        return reverseList(newHead);
+
+
+
+    }
+    public static ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+}
